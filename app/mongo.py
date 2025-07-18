0333ccabca3ad1a6c5107dc4180468506d909312
Atharv Sharma <atharv5873@gmail.com>
2025-07-19 03:11:28 +0530

    chore: add requirements.txt and setup MongoDB connection in mongo.py

---
+++ app/mongo.py
@@ -0,0 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+client = AsyncIOMotorClient(MONGODB_URI)
+db = client.phishing_db
+detections = db.detections
